293e5ca0ba5301672b3105bfb312f1704c6d6831
militist <neumpanin@yandex.ru>
2023-03-22 10:07:24 +0300

    добавлена задача 1

---
+++ Hausaufgabe_5/aufgabe_1.java
@@ -0,0 +1,39 @@
+// Реализуйте структуру телефонной книги с помощью HashMap, учитывая, что 1 человек может иметь несколько телефонов.
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class aufgabe_1{
+
+       // Метод, который добавляет номера в книгу
+    public static void addNumber(String key, int value, Map<String, ArrayList<Integer>> map){
+        if (map.containsKey(key)) {
+            map.get(key).add(value);
+        } else {
+            ArrayList<Integer> list = new ArrayList<>();
+            list.add(value);
+            map.put(key, list);
+        }
+
+    }
+    // Метод, который печатает список контактов
+    public static void printBook(Map<String, ArrayList<Integer>> map){
+        for (var item : map.entrySet()) {
+            String phones = "";
+            for(int el: item.getValue()){
+                phones = phones + el + ", ";
+            }
+            System.out.printf("%s: %s \n", item.getKey(), phones);
+        }
+    }
+    public static void main(String[] args) {
+        Map<String, ArrayList<Integer>> bookPhone = new HashMap<>();
+        addNumber("Орлов", 425698, bookPhone);
+        addNumber("Сорокин", 569578, bookPhone);
+        addNumber("Воронов", 546585, bookPhone);
+        addNumber("Орлов", 95647722, bookPhone);
+        addNumber("Сорокин", 451278, bookPhone);
+        addNumber("Соколов", 965214, bookPhone);
+        printBook(bookPhone);
+       }
+}
